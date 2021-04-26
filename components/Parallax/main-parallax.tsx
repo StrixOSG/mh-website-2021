667eb6044fad85f02589860bbb8d04452ddbedfd
StrixOSG <6631874+StrixOSG@users.noreply.github.com>
2021-04-25 21:36:52 -0600

    feat: first section done (not mobile friendly yet), added lots of svgs

--- components/Parallax/main-parallax.tsx
+++ components/Parallax/main-parallax.tsx
@@ -1,6 +1,6 @@
 import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax';
 import React, { useRef } from "react";
-import './main-parallax.module.css';
+import styles from './main-parallax.module.css';
 
 const url = (name: string, wrap = false) =>
   `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
@@ -9,7 +9,7 @@ const url = (name: string, wrap = false) =>
 export default function MainParallax() {
     const parallax = useRef<IParallax>(null!)
     return (
-        <Parallax ref={parallax} pages={3} style={{ width: '100%', height: '100%', background: '#253237' }}>
+        <Parallax ref={parallax} pages={3} style={{ width: '100%', height: '100%', background: '#340068' }}>
             <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#805E73' }} />
             <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#87BCDE' }} />
 
@@ -54,18 +54,6 @@ export default function MainParallax() {
             </ParallaxLayer>
 
             <ParallaxLayer
-            offset={2.5}
-            speed={-0.4}
-            style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                pointerEvents: 'none',
-            }}>
-            <img src={url('earth')} style={{ width: '60%' }} />
-            </ParallaxLayer>
-
-            <ParallaxLayer
             offset={2}
             speed={-0.3}
             style={{
@@ -74,18 +62,6 @@ export default function MainParallax() {
                 backgroundImage: url('clients', true),
             }}
             />
-
-            <ParallaxLayer
-            offset={0}
-            speed={0.1}
-            onClick={() => parallax.current.scrollTo(0.5)}
-            style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-            }}>
-            <img src={url('server')} style={{ width: '20%' }} />
-            </ParallaxLayer>
 
             <ParallaxLayer
             offset={1}
@@ -109,6 +85,71 @@ export default function MainParallax() {
             }}
             onClick={() => parallax.current.scrollTo(0)}>
             <img src={url('clients-main')} style={{ width: '40%' }} />
+            </ParallaxLayer>
+            <div style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        pointerEvents: 'none',
+                    }}
+            >
+                <ParallaxLayer
+                    offset={0}
+                    speed={0.2}
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        pointerEvents: 'none',
+                    }}>
+                    <div style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            pointerEvents: 'none',
+                        }}
+                    >
+                        <h1 className={styles.spaceTextMain}>WEB | APP | GAME</h1>
+                        <h1 className={styles.spaceTextMain}>DEVELOPMENT & DESIGNS</h1>
+                    </div>
+                </ParallaxLayer>
+                <ParallaxLayer
+                    offset={0}
+                    speed={0.2}
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'left',
+                        pointerEvents: 'none',
+                    }}>
+                    <div className={styles.bitmojiSpaceContainer}>
+                        <p className={styles.bitmojiSpaceText} style={{position: 'absolute', left: '15rem'}}>Hi, I'm Matt</p>
+                        <img className={styles.bitmojiSpace} src="/bitmoji/bitmoji-space.svg"/>
+                    </div>
+                </ParallaxLayer>
+            </div>
+            <ParallaxLayer 
+                offset={0.32}
+                speed={-0.1} 
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    pointerEvents: 'none',
+                    opacity: 0.4 
+                }}>
+                <img src="/Cloud-1.svg" style={{ width: '700px' }} />
+            </ParallaxLayer>
+            <ParallaxLayer 
+                offset={0.43}
+                speed={-0.1} 
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'flex-end',
+                    pointerEvents: 'none',
+                    opacity: 0.4 
+                }}>
+                <img src="/Cloud-2.svg" style={{ width: '900px', marginRight: '100px' }} />
             </ParallaxLayer>
         </Parallax>
     )
