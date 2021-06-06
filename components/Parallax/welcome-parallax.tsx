5566d1c5df2687fc13e172fef96ec781c99e43fa
StrixOSG <6631874+StrixOSG@users.noreply.github.com>
2021-06-05 22:13:58 -0600

    refactor: adjusted stylistic alignment

--- components/Parallax/welcome-parallax.tsx
+++ components/Parallax/welcome-parallax.tsx
@@ -5,8 +5,8 @@ import styles from './main-parallax.module.css';
 export default function WelcomeParallax() {
     return (
         <div>
-            <ParallaxLayer offset={0} speed={0.2} className="flex flex-col xl:flex-row justify-center">
-                <div className="flex flex-col justify-center order-2 items-center xl:order-1">
+            <ParallaxLayer offset={0} speed={0.2} className="flex flex-col justify-center xl:flex-row">
+                <div className="flex flex-col justify-center order-2 items-center mt-16 xl:mt-0 xl:order-1 xl:mr-32">
                     <div className={styles.bitmojiSpaceContainer}>
                         <div className={styles.bitmojiSpaceText}>
                             <p className="absolute text-xl left-28 top-10 xl:left-52 xl:text-3xl xl:top-12">Hi, I'm Matt</p>
@@ -16,9 +16,9 @@ export default function WelcomeParallax() {
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col items-center justify-end text-3xl order-1 xl:order-2 md:text-5xl lg:text-7xl xl:justify-center">
+                <div className="flex flex-col items-center justify-end text-center text-3xl order-1 xl:order-2 md:text-5xl xl:text-7xl xl:justify-center xl:mr-80">
                     <h1 className="text-white">WEB | APP | GAME</h1>
-                    <h1 className="mt-1 text-white">DEVELOPMENT & DESIGNS</h1>
+                    <h1 className="mt-1 text-white">DEVELOPMENT & DESIGN</h1>
                 </div>
             </ParallaxLayer>
             <ParallaxLayer offset={0.43} speed={0.6} className="flex justify-end opacity-40">
