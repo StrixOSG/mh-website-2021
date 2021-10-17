4e6a825f0445312d91122d658757f5ce67eccfeb
StrixOSG <6631874+StrixOSG@users.noreply.github.com>
2021-10-16 21:02:43 -0600

    fix: parallax works much better now

--- components/Parallax/main-parallax.tsx
+++ components/Parallax/main-parallax.tsx
@@ -4,17 +4,18 @@ import SkillsParallax from './skills-parallax';
 import WelcomeParallax from './welcome-parallax';
 
 const url = (name: string, wrap = false) =>
-  `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
+    `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
 
 
 export default function MainParallax() {
     const parallax = useRef<IParallax>(null!)
     return (
-        <Parallax ref={parallax} pages={3} style={{ background: '#B1D4ED' }}>
-            <ParallaxLayer offset={0} speed={1} style={{ background: '#340068' }} />
-            <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#FF6978' }} />
-            <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#B1EDE8' }} />
-            <ParallaxLayer offset={0} speed={0} factor={3} style={{ backgroundImage: url('stars', true), backgroundSize: 'cover' }}/>
+        <Parallax ref={parallax} pages={4}>
+            <ParallaxLayer offset={0} speed={0} style={{ background: '#340068' }} />
+            <ParallaxLayer offset={1} speed={0} style={{ background: '#B1D4ED' }} />
+            <ParallaxLayer offset={2} speed={0} style={{ backgroundColor: '#FF6978' }} />
+            <ParallaxLayer offset={3} speed={0} style={{ backgroundColor: '#B1EDE8' }} />
+            <ParallaxLayer offset={0} speed={0} factor={4} style={{ backgroundImage: url('stars', true), backgroundSize: 'cover' }} />
             <WelcomeParallax></WelcomeParallax>
             <SkillsParallax></SkillsParallax>
         </Parallax>
--- components/Parallax/skills-parallax.tsx
+++ components/Parallax/skills-parallax.tsx
@@ -7,7 +7,7 @@ import { IconPath } from '../../constants/icon-path';
 export default function SkillsParallax() {
     return (
         <div>
-            <ParallaxLayer offset={0.9} speed={1} className="grid grid-rows-1 place-items-center xl:grid-cols-2 xl:grid-rows-none">
+            <ParallaxLayer offset={1} speed={1} className="grid grid-rows-1 place-items-center xl:grid-cols-2 xl:grid-rows-none">
                 <div className="hidden xl:flex xl:flex-col xl:justify-center xl:items-center xl:my-16 xl:mt-0 xl:mr-32">
                     <img className={`${styles.owl} w-3/12 sm:w-2/12 lg:w-2/12 xl:w-5/12 mb-6`} src="/owls/owl-hover.svg" />
                     <img className="w-7/12 sm:w-5/12 lg:w-4/12 xl:w-full" src="/bitmoji/bitmoji-surprise.svg" />
