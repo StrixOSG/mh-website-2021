ea5a6bc217d2af926e54bb145d4a2fbd7d2180de
StrixOSG <6631874+StrixOSG@users.noreply.github.com>
2021-10-17 10:55:25 -0600

    feat: added skills and interests and adjusted padding

--- components/Parallax/skills-parallax.tsx
+++ components/Parallax/skills-parallax.tsx
@@ -15,51 +15,51 @@ export default function SkillsParallax() {
                 <div className="text-center text-3xl px-4 md:text-5xl xl:justify-self-start md:flex xl:text-7xl">
                     <div className="bg-white rounded-md sm:w-96 mt-4 md:mt-0 md:mr-4">
                         <div className="bg-purple rounded-t-md h-2"></div>
-                        <div className="mt-4 text-3xl sm:text-4xl text-gray-600">
+                        <div className="py-4 text-3xl sm:text-4xl text-gray-600">
                             <p>Development</p>
                             <p>Experience</p>
-                        </div>
-                        <div className="mt-6 font-serif text-2xl sm:text-3xl">
-                            <p className="text-purple mb-2">Languages</p>
-                            <div className="grid grid-cols-6 justify-center items-center px-6">
-                                <Icon iconPath={IconPath.angular}></Icon>
-                                <Icon iconPath={IconPath.c}></Icon>
-                                <Icon iconPath={IconPath.cplusplus}></Icon>
-                                <Icon iconPath={IconPath.csharp}></Icon>
-                                <Icon iconPath={IconPath.css}></Icon>
-                                <Icon iconPath={IconPath.html}></Icon>
-                                <Icon iconPath={IconPath.javascript}></Icon>
-                                <Icon iconPath={IconPath.jquery}></Icon>
-                                <Icon iconPath={IconPath.laravel}></Icon>
-                                <Icon iconPath={IconPath.mysql}></Icon>
-                                <Icon iconPath={IconPath.nestjs}></Icon>
-                                <Icon iconPath={IconPath.nodejs}></Icon>
-                                <Icon iconPath={IconPath.php}></Icon>
-                                <Icon iconPath={IconPath.postgresql}></Icon>
-                                <Icon iconPath={IconPath.typescript}></Icon>
-                                <Icon iconPath={IconPath.react}></Icon>
-                                <Icon iconPath={IconPath.sass}></Icon>
+                            <div className="mt-6 font-serif text-2xl sm:text-3xl">
+                                <p className="text-purple mb-2">Languages</p>
+                                <div className="grid grid-cols-6 justify-center items-center px-6">
+                                    <Icon iconPath={IconPath.angular}></Icon>
+                                    <Icon iconPath={IconPath.c}></Icon>
+                                    <Icon iconPath={IconPath.cplusplus}></Icon>
+                                    <Icon iconPath={IconPath.csharp}></Icon>
+                                    <Icon iconPath={IconPath.css}></Icon>
+                                    <Icon iconPath={IconPath.html}></Icon>
+                                    <Icon iconPath={IconPath.javascript}></Icon>
+                                    <Icon iconPath={IconPath.jquery}></Icon>
+                                    <Icon iconPath={IconPath.laravel}></Icon>
+                                    <Icon iconPath={IconPath.mysql}></Icon>
+                                    <Icon iconPath={IconPath.nestjs}></Icon>
+                                    <Icon iconPath={IconPath.nodejs}></Icon>
+                                    <Icon iconPath={IconPath.php}></Icon>
+                                    <Icon iconPath={IconPath.postgresql}></Icon>
+                                    <Icon iconPath={IconPath.typescript}></Icon>
+                                    <Icon iconPath={IconPath.react}></Icon>
+                                    <Icon iconPath={IconPath.sass}></Icon>
+                                </div>
                             </div>
-                        </div>
-                        <div className="my-6 font-serif text-2xl sm:text-3xl">
-                            <p className="text-purple mb-2">Tools</p>
-                            <div className="grid grid-cols-5 justify-center items-center px-6">
-                                <Icon iconPath={IconPath.aws}></Icon>
-                                <Icon iconPath={IconPath.docker}></Icon>
-                                <Icon iconPath={IconPath.github}></Icon>
-                                <Icon iconPath={IconPath.heroku}></Icon>
-                                <Icon iconPath={IconPath.unity}></Icon>
-                                <Icon iconPath={IconPath.webpack}></Icon>
-                                <Icon iconPath={IconPath.yarn}></Icon>
+                            <div className="my-6 mb-4 font-serif text-2xl sm:text-3xl">
+                                <p className="text-purple mb-2">Tools</p>
+                                <div className="grid grid-cols-5 justify-center items-center px-6">
+                                    <Icon iconPath={IconPath.aws}></Icon>
+                                    <Icon iconPath={IconPath.docker}></Icon>
+                                    <Icon iconPath={IconPath.github}></Icon>
+                                    <Icon iconPath={IconPath.heroku}></Icon>
+                                    <Icon iconPath={IconPath.unity}></Icon>
+                                    <Icon iconPath={IconPath.webpack}></Icon>
+                                    <Icon iconPath={IconPath.yarn}></Icon>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="bg-white rounded-md w-80 mt-4 md:mt-0">
+                    <div className="bg-white rounded-md w-96 mt-4 md:mt-0">
                         <div className="bg-teal rounded-t-md h-2"></div>
-                        <div className="mt-4 text-xl sm:text-4xl text-gray-600">
+                        <div className="py-4 text-3xl sm:text-4xl text-gray-600">
                             <p>Design & General</p>
                             <p>Experience</p>
-                            <div className="mt-6 font-serif text-2xl sm:text-3xl">
+                            <div className="font-serif text-2xl mt-2 sm:mt-6 sm:text-3xl">
                                 <p className="text-teal mb-2">Tools</p>
                                 <div className="grid grid-cols-5 justify-center items-center px-6">
                                     <Icon iconPath={IconPath.aftereffects}></Icon>
@@ -71,11 +71,14 @@ export default function SkillsParallax() {
                                     <Icon iconPath={IconPath.figma}></Icon>
                                 </div>
                             </div>
-                            <div className="mt-6 font-serif text-2xl sm:text-3xl">
+                            <div className="font-serif text-2xl mt-2 sm:mt-6 sm:text-3xl">
                                 <p className="text-teal mb-2">Skills & Interests</p>
-                                <div className="grid grid-cols-5 justify-center items-center px-6">
-                                    {/* List of skills and interests */}
-                                </div>
+                                <ul className="list-disc text-lg text-left ml-10">
+                                    <li>Full Stack Dev, UX, AR</li>
+                                    <li>Learning New Tech</li>
+                                    <li>Growing Startups</li>
+                                    <li>Team & Task Management</li>
+                                </ul>
                             </div>
                         </div>
                     </div>
