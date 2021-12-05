353a0eb9026e0637d259781319fe0e7ce604dab8
StrixOSG <6631874+StrixOSG@users.noreply.github.com>
2021-12-05 13:05:21 -0600

    fix: footer adjusted for mobile

--- components/Footer/footer.tsx
+++ components/Footer/footer.tsx
@@ -4,9 +4,9 @@ import Icon from "../Shared/icon";
 
 export default function Footer() {
     return (
-        <div className="absolute bottom-0 right-10 flex flex-col justify-center items-center">
-            <p className="font-serif text-2xl text-white opacity-70">LET'S CONNECT</p>
-            <div className="flex justifty-center items-center">
+        <div className="flex flex-col justify-center items-center absolute bottom-0 right-2 sm:right-10">
+            <p className="font-serif text-white opacity-70 text-xl sm:text-2xl">LET'S CONNECT</p>
+            <div className="hidden sm:flex justifty-center items-center">
                 <a target="_blank" href="https://github.com/StrixOSG" rel="noopener noreferrer" tabIndex={0}>
                     <Icon iconPath={IconPath.github} width={50} height={50}></Icon>
                 </a>
@@ -15,6 +15,17 @@ export default function Footer() {
                 </a>
                 <a href="mailto:matthewhamiltonweb@gmail.com">
                     <Icon iconPath={IconPath.email} width={50} height={50}></Icon>
+                </a>
+            </div>
+            <div className="flex sm:hidden justifty-center items-center">
+                <a target="_blank" href="https://github.com/StrixOSG" rel="noopener noreferrer" tabIndex={0}>
+                    <Icon iconPath={IconPath.github} width={40} height={40}></Icon>
+                </a>
+                <a target="_blank" href="https://www.linkedin.com/in/matthew-hamilton-aa3271108/" rel="noopener noreferrer" tabIndex={0}>
+                    <Icon iconPath={IconPath.linkedIn} width={40} height={40}></Icon>
+                </a>
+                <a href="mailto:matthewhamiltonweb@gmail.com">
+                    <Icon iconPath={IconPath.email} width={40} height={40}></Icon>
                 </a>
             </div>
         </div>
